cdb397d14c426a6f8b5afe90c7d7e4ffafe04390
WhoKnows <alu0100918205@ull.edu.es>
2023-12-17 18:08:30 +0000

    Update FlightTest.java

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -1,21 +1,38 @@
+/**
+ * @file
+ * @brief Contiene pruebas unitarias para la clase Flight.
+ */
+
 import es.ull.passengers.Passenger;
 import es.ull.flights.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @brief Pruebas unitarias para la clase Flight.
+ */
 class FlightTest {
 
+    /**
+     * @brief Verifica que se cree correctamente un vuelo con un número válido.
+     */
     @Test
     void testValidFlightNumber() {
         Flight flight = new Flight("AB123", 100);
         assertEquals("AB123", flight.getFlightNumber());
     }
 
+    /**
+     * @brief Verifica que se lance una excepción al intentar crear un vuelo con un número inválido.
+     */
     @Test
     void testInvalidFlightNumber() {
         assertThrows(RuntimeException.class, () -> new Flight("InvalidFlightNumber", 100));
     }
 
+    /**
+     * @brief Verifica que se añadan correctamente pasajeros a un vuelo y se actualice el número de pasajeros.
+     */
     @Test
     void testAddPassenger() {
         Flight flight = new Flight("AB123", 2);
@@ -28,6 +45,9 @@ class FlightTest {
         assertEquals(2, flight.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Verifica que se lance una excepción al intentar añadir un pasajero a un vuelo lleno.
+     */
     @Test
     void testAddPassengerExceedingSeats() {
         Flight flight = new Flight("AB123", 1);
@@ -40,17 +60,11 @@ class FlightTest {
         assertEquals("Not enough seats for flight AB123", exception.getMessage());
     }
 
+    /**
+     * @brief Verifica que se elimine correctamente un pasajero de un vuelo y se actualice el número de pasajeros.
+     */
     @Test
     void testRemovePassenger() {
         Flight flight = new Flight("AB123", 2);
         Passenger passenger1 = new Passenger("John", "Doe", "US");
-        Passenger passenger2 = new Passenger("Jane", "Doe", "US");
-
-        flight.addPassenger(passenger1);
-        flight.addPassenger(passenger2);
-
-        assertTrue(flight.removePassenger(passenger1));
-
-        assertEquals(1, flight.getNumberOfPassengers());
-    }
-}
+        Passenger passenger2 = new Passenger("Jane", "Doe", "US
