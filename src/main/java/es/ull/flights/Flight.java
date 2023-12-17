c2ca87ae53e195da0432913b2eb4e66d4628af01
WhoKnows <alu0100918205@ull.edu.es>
2023-12-17 18:03:36 +0000

    Update Flight.java

--- src/main/java/es/ull/flights/Flight.java
+++ src/main/java/es/ull/flights/Flight.java
@@ -18,6 +18,7 @@
  *
  * ========================================================================
  */
+
 package es.ull.flights;
 
 import java.util.HashSet;
@@ -27,15 +28,24 @@ import java.util.regex.Pattern;
 
 import es.ull.passengers.Passenger;
 
+/**
+ * @brief Represents a flight with a flight number, seat capacity, and a set of passengers.
+ */
 public class Flight {
 
-    private String flightNumber;
-    private int seats;
-    private Set<Passenger> passengers = new HashSet<>();
+    private String flightNumber;  ///< Flight number in the format [A-Z]{2}\d{3,4}.
+    private int seats;  ///< Number of available seats on the flight.
+    private Set<Passenger> passengers = new HashSet<>();  ///< Set of passengers on the flight.
 
-    private static String flightNumberRegex = "^[A-Z]{2}\\d{3,4}$";
-    private static Pattern pattern = Pattern.compile(flightNumberRegex);
+    private static String flightNumberRegex = "^[A-Z]{2}\\d{3,4}$";  ///< Regular expression for valid flight numbers.
+    private static Pattern pattern = Pattern.compile(flightNumberRegex);  ///< Compiled pattern for flight number validation.
 
+    /**
+     * @brief Constructs a Flight object with the specified flight number and seat capacity.
+     * @param flightNumber The flight number to be set.
+     * @param seats The number of seats available on the flight.
+     * @throws RuntimeException if the provided flight number is invalid.
+     */
     public Flight(String flightNumber, int seats) {
         Matcher matcher = pattern.matcher(flightNumber);
         if (!matcher.matches()) {
@@ -45,18 +55,36 @@ public class Flight {
         this.seats = seats;
     }
 
+    /**
+     * @brief Gets the flight number.
+     * @return The flight number.
+     */
     public String getFlightNumber() {
         return flightNumber;
     }
 
+    /**
+     * @brief Gets the number of available seats on the flight.
+     * @return The number of seats.
+     */
     public int getSeats() {
         return seats;
     }
 
+    /**
+     * @brief Gets the number of passengers on the flight.
+     * @return The number of passengers.
+     */
     public int getNumberOfPassengers() {
         return passengers.size();
     }
 
+    /**
+     * @brief Adds a passenger to the flight.
+     * @param passenger The passenger to be added.
+     * @return True if the passenger was added successfully, false otherwise.
+     * @throws RuntimeException if there are not enough seats for the flight.
+     */
     public boolean addPassenger(Passenger passenger) {
         if (getNumberOfPassengers() >= seats) {
             throw new RuntimeException("Not enough seats for flight " + getFlightNumber());
@@ -65,6 +93,11 @@ public class Flight {
         return passengers.add(passenger);
     }
 
+    /**
+     * @brief Removes a passenger from the flight.
+     * @param passenger The passenger to be removed.
+     * @return True if the passenger was removed successfully, false otherwise.
+     */
     public boolean removePassenger(Passenger passenger) {
         passenger.setFlight(null);
         return passengers.remove(passenger);
