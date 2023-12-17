1428b18d71b78fa9cc38e9c0f0f9db1fe59d5ecd
WhoKnows <alu0100918205@ull.edu.es>
2023-12-17 18:09:43 +0000

    Update PassengerTest.java

--- src/test/java/es/ull/passengers/PassengerTest.java
+++ src/test/java/es/ull/passengers/PassengerTest.java
@@ -4,8 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @file
+ * @brief Contiene pruebas para la clase Passenger.
+ */
 class PassengerTest {
 
+    /**
+     * @brief Verifica la creación válida de un objeto Passenger.
+     */
     @Test
     void testValidPassengerCreation() {
         Passenger passenger = new Passenger("John123", "John Doe", "US");
@@ -16,6 +23,9 @@ class PassengerTest {
         assertNull(passenger.getFlight());
     }
 
+    /**
+     * @brief Verifica la unión de un pasajero a un vuelo.
+     */
     @Test
     void testJoinFlight() {
         Passenger passenger = new Passenger("Jane456", "Jane Doe", "US");
@@ -28,6 +38,9 @@ class PassengerTest {
         assertTrue(flight.getNumberOfPassengers() <= flight.getSeats());
     }
 
+    /**
+     * @brief Verifica que un pasajero pueda abandonar un vuelo.
+     */
     @Test
     void testLeaveFlight() {
         Passenger passenger = new Passenger("Alice789", "Alice Smith", "US");
@@ -40,7 +53,9 @@ class PassengerTest {
         assertEquals(0, flight.getNumberOfPassengers());
     }
 
-
+    /**
+     * @brief Verifica que se lance una excepción al intentar crear un pasajero con un código de país inválido.
+     */
     @Test
     void testInvalidCountryCode() {
         assertThrows(RuntimeException.class, () -> new Passenger("InvalidID", "Invalid Name", "XX"));
